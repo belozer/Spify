a13c261838b7fb32aa84cf291636e8e6d745312b
Sergey Belozyorcev <nextdrift@gmail.com>
2014-11-27 21:08:09 +0300

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+#Spif
+#### Light (spif < 1 Kb) and sample jQuery plugin for sprite animations
+
+### 1.Getting Started
+Load jQuery(1.7+) and include spif plugin
+
+```html
+<script src="jquery.js"></script>
+<script src="jquery.spif.js"></script>
+```
+### 2.Set up your HTML
+
+```html
+<div class="animation"></div>
+```
+### 3.Call the plugin
+Now call the spif initializer function and your animation is ready.
+
+```js
+$(".animation").spif({
+    frame: [100, 100],
+    frames: 4,
+    fps: 5,
+    url: 'sprite.png'
+});
+```
+
+### Settings
+```js
+    frame:  array     // Sizes frame [width, height]
+
+    frames: int       // Total count frames.
+                      // Default: 1
+
+    fps:    int       // Speed animation.
+                      // Default: 5
+
+    url:    string    // Url to sprite image
+
+    vector: string    // Position frames in sprite (vertical or horizontal).
+                      // Default: vertical
+```
+License
+------------
+The MIT License (MIT)
+
+Copyright (c) 2014 Sergey Belozyorcev
---
+++ jquery.spif.js
@@ -0,0 +1,57 @@
+/*
+* jQuery Spif v0.1
+*
+* Copyright (c) 2014 Sergey Belozyorcev
+* http://vk.com/s.belozyorcev/
+*
+* Licensed under MIT
+*
+*/
+(function( $ ){
+
+$.fn.spif = function( options ) {
+
+    var settings = $.extend( {
+        'fps'    : 5,
+        'frames' : 1,
+        'url'    : '',
+        'frame'  : [100, 100],
+        'vector' : 'vertical'
+    }, options);
+
+    var time = Math.floor( 1000 / settings.fps );
+
+    return this.each(function() {
+
+        var el = $(this);
+
+        var currentIndex = 0;
+        var bgPos;
+
+        function circle () {
+            currentIndex += 1;
+            if (currentIndex >= settings.frames) { currentIndex = 0; }
+
+            if (settings.vector == 'vertical') {
+                bgPos = settings.frame[1] * currentIndex;
+                bgPos = '0px ' + bgPos + 'px';
+            } else {
+                bgPos = settings.frame[0] * currentIndex + 'px 0px';
+            }
+
+            console.log(bgPos);
+            el.css('background-position', bgPos );
+        }
+
+        $(this).css({
+            width:  settings.frame[0],
+            height: settings.frame[1],
+            background: 'url(' + settings.url + ')'
+        });
+
+        setInterval(circle, time);
+
+    });
+
+};
+})( jQuery );
